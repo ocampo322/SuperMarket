e87c3510eefaf4256a0b35063eeb9c4c07a8cd41
Juan Sebastian Ocampo Salazar <ocampojuan035@gmail.com>
2024-10-12 22:02:20 -0500

    inicialize el crud customers y el formulario customers

---
+++ SQLQuery4.sql
@@ -0,0 +1,13 @@
+﻿
+use Supermarket;
+
+create table Customer (
+    Customer_Id INT PRIMARY KEY,
+    Customer_Document_Number CHAR(15) NOT NULL UNIQUE,
+    Customer_First_Name CHAR(50) NOT NULL,
+    Customer_Last_Name CHAR(50) NOT NULL,
+    Customer_Address VARCHAR(80),
+    Customer_Birthday DATETIME,
+    Customer_Phone_Number CHAR(16),
+    Customer_Email VARCHAR(100)
+)
