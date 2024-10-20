8f0feaf2776a645fd2b3c1511509933e5a163b79
Juan Sebastian Ocampo Salazar <ocampojuan035@gmail.com>
2024-10-20 17:34:01 -0500

    Categories View

---
+++ SQLQuery6.sql
@@ -0,0 +1,11 @@
+﻿use Supermarket
+
+create table Categories
+
+(
+    Categories_Id INT IDENTITY(1,1) PRIMARY KEY,
+    Categories_Name NVARCHAR(80) NOT NULL,
+    Categories_Description NVARCHAR(80) NOT NULL
+)
+GO
+
